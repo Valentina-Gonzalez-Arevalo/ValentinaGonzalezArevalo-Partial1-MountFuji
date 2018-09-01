c0947e19250b483926537ac1568b8193ad1afa82
D. Valentina Gonzalez Arevalo <valeg981000@gmail.com>
2018-09-01 13:53:48 -0500

    Cámara no rotada en 45º sino en un plano mejor

---
+++ Assets/Scripts/SimpleMotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMotion : MonoBehaviour {
+
+    public float translationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(translationSpeed, 0, 0);
+		
+	}
+}
